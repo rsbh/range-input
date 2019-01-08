579f7f40dd4a1667e57857064379e818e2f7af3b
Rishabh Mishra <mishrarishabh865@gmail.com>
2019-01-08 14:23:46 +0530

    add function to handle input

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log("hello world");
+const inputBox = document.getElementById("input-box");
+
+const getValues = value => {
+  console.log(value);
+  if (value.indexOf(",") > -1) {
+    return value.split(",").map(Number);
+  } else if (value.indexOf("-") > -1) {
+    const range = value.split("-");
+    const start = Number(range[0]);
+    const end = Number(range[1]);
+    const list = [];
+    for (let i = start; i <= end; i++) {
+      list.push(i);
+    }
+    return list;
+  }
+  const num = Number(value);
+  return [num];
+};
+
+const handleInputChange = event => {
+  const value = event.target.value;
+  const valueArr = getValues(value);
+  console.log(valueArr);
+};
+
+inputBox.addEventListener("change", handleInputChange);
