9964c0a15899b14dfb7f969559e651a68577f7c4
Rishabh Mishra <mishrarishabh865@gmail.com>
2019-01-08 18:18:08 +0530

    add parser for range comma and number

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 const inputBox = document.getElementById("input-box");
 
-const getValues = value => {
-  console.log(value);
-  if (value.indexOf(",") > -1) {
-    return value.split(",").map(Number);
-  } else if (value.indexOf("-") > -1) {
+const flatArr = arr => arr.reduce((acc, val) => acc.concat(val), []);
+
+const rangeParser = value => {
+  if (value.indexOf("-") > -1) {
     const range = value.split("-");
     const start = Number(range[0]);
     const end = Number(range[1]);
@@ -14,9 +13,24 @@ const getValues = value => {
     }
     return list;
   }
+  return null;
+};
+
+const commaParser = value => {
+  if (value.indexOf(",") > -1) {
+    return flatArr(value.split(",").map(getValues));
+  }
+  return null;
+};
+
+const numberParser = value => {
   const num = Number(value);
   return [num];
 };
+
+const anyOneParserFactory = (...parsers) => input =>
+  parsers.reduce((accum, parser) => (accum === null ? parser(input) : accum), null);
+const getValues = anyOneParserFactory(commaParser, rangeParser, numberParser);
 
 const handleInputChange = event => {
   const value = event.target.value;
@@ -24,4 +38,8 @@ const handleInputChange = event => {
   console.log(valueArr);
 };
 
-inputBox.addEventListener("change", handleInputChange);
+// inputBox.addEventListener("change", handleInputChange);
+
+module.exports = {
+  getValues
+};
